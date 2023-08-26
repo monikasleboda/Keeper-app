f4c6b38f1cf601eb1a390e3f99e634bcb3f0cfb1
Monika Śleboda <monikakozak60@gmail.com>
2023-08-26 08:00:11 +0200

    Add DELETE button to notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -5,8 +5,9 @@ function Note(props){
         <div class="note">
                 <h1>{props.title}</h1>
                 <p>{props.content}</p>
+                <button>DELETE</button>
         </div>
-    )
-};
+    );
+}
 
 export default Note;
