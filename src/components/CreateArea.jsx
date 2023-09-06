2a9bc0c1aef1283416de6c751d306d5e0b3e9184
Monika Śleboda <monikakozak60@gmail.com>
2023-09-06 14:19:30 +0200

    Style the Keeper App

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,12 @@
 import React, {useState} from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props){
+
+    const [isExpanded, setExpanded] = useState(false);
+
     const [note, setNote] = useState({
         title: "",
         content: ""
@@ -26,22 +32,34 @@ function CreateArea(props){
         event.preventDefault();
     }
 
+    function expand(){
+        setExpanded(true);
+    }
 
     return(
         <div>
-            <form>
-                <input 
-                    onChange={handleChange}
-                    name="title" 
-                    value={note.title} 
-                    placeholder="Title" />
+            <form className="create-note">
+                {isExpanded && (
+                    <input 
+                        onChange={handleChange}
+                        name="title" 
+                        value={note.title} 
+                        placeholder="Title" 
+                    />
+                 )}
                 <textarea
+                    onClick={expand}
                     onChange={handleChange}
                     name="content" 
                     value={note.content} 
                     placeholder="Take a note..." 
-                    rows="3" />
-                <button onClick={submitNote} >Add</button>
+                    rows={isExpanded ? 3 : 1}
+                />
+                <Zoom in={isExpanded}>
+                    <Fab onClick={submitNote} >
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
             </form>
         </div>
     );
